214bf290e562b1011a63490eda268282ed2f16f3
Talha Rashid <mtr04412@gmail.com>
2025-08-06 15:25:02 +0400

    Google Weather API
    I ask user to enter a city name and show weather report of that city using Google Weather API

---
+++ weather.py
@@ -0,0 +1,38 @@
+# ------------------------------------- Google Weather API -----------------------------------
+import requests
+
+city = input("Enter the city name: ")
+
+API_KEY = 'd993c1b134e6b539aa1bad9e927d17e3'
+# city = 'Dubai'
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+response = requests.get(url)
+data = response.json()
+
+for key, value in data.items():
+    print(f"{key.capitalize()}: {value}")
+    
+print("---------------------------------------------------------------------------------")
+print("----------------------------- 2nd API -------------------------------------------")
+print("---------------------------------------------------------------------------------")
+
+# ------------------------------------- Open Meteo Weather API -----------------------------------
+
+# add 'long' and 'lat' of Dubai
+
+latitude = 25.276987
+longitude = 55.296249
+
+url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true"
+
+response = requests.get(url)
+data = response.json()
+data = data['current_weather']
+
+for result in data:
+    print(result.capitalize(), data[result])
+
+for key, value in data.items():
+    print(f"{key.capitalize()}: {value}")
+
